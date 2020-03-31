11c4880ae2193fcb7f4f986d527987a00bd84cef
Emanuel Rodriguez <joseemaro@hotmail.com>
2020-03-31 15:10:17 -0300

    me falla el pasaje por parametros con el rmi

---
+++ punto6-corregir/src/main/java/Tp_1/punto6/ServerImplement.java
@@ -0,0 +1,43 @@
+package Tp_1.punto6;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+
+
+
+public class ServerImplement implements RemoteInt{
+
+
+
+	public int[] sumarVector(int[] v1, int[] v2) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	    int[] v3 = new int[5];
+		for (int i=0; i<v1.length; i++) {
+			v3[i] = v1[i]+v2[i];
+		}
+		return v3;
+	}
+
+	public int[] devolverVector(int[] v1) throws RemoteException {
+		// TODO Auto-generated method stub
+		for (int i=0; i<v1.length; i++) {
+			v1[i]=0;
+		}
+		return v1;
+	}
+
+
+	public int[] sumarVect(Vector v4, Vector v5) throws RemoteException {
+		int[] v3 = new int[5];
+		/*int[] a1= v4.getV1();
+		int[] a2 = v5.getV1();
+		for (int i=0; i<a1.length; i++) {
+			v3[i] = a1[i]+a2[i];
+			//v4.getV1()[i]= 0;
+			//v5.getV1()[i]= 0;
+		}*/
+		return v3;
+	}
+}
